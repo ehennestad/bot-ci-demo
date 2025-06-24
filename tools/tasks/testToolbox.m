5175b60272936de6b0b87838f458ebcbcc2a3387
ehennestad <ehennestad@gmail.com>
2025-06-24 16:04:29 +0200

    Update prefs to use for testing.

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -1,10 +1,20 @@
 function testToolbox(varargin)
     projectRootDir = bottools.projectdir();
-    prefs = bot.util.getPreferences;
-    prefs.CacheDirectory = fullfile(fileparts(projectRootDir), 'bot_cache');
-    if ~isfolder(prefs.CacheDirectory); mkdir(prefs.CacheDirectory); end
-    prefs.DialogMode = "Command Window";
-
+    
+    botPrefs = bot.util.getPreferences();
+    if botPrefs.CacheDirectory == "" || ~isfolder(botPrefs.CacheDirectory)
+        botPrefs.DialogMode = "Command Window";
+        tempCache = fullfile(tempdir, 'bot_cache');
+        if ~isfolder(tempCache)
+            fprintf('Making cache directory "%s"', tempCache)
+            mkdir(tempCache); 
+        end
+        cleanupObj = onCleanup(@(fp) rmdir(tempCache, "s"));
+        botPrefs.CacheDirectory = tempCache;
+        botPrefs.AutoDownloadNwb = false;
+        disp(botPrefs)
+    end
+    
     matbox.tasks.testToolbox(projectRootDir, ...
         "SourceFolderName", "src", ...
         "TestsFolderName", "tests", varargin{:})
